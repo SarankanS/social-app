593532fb9de4bee42d5c41420f18f464d14307b3
Sarankan Srikaran <sarankansrikaran@gmail.com>
2026-01-14 13:52:53 -0500

    fix: load environment variables in db pool connection

--- odin-book-server/db/pool.js
+++ odin-book-server/db/pool.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
 const { Pool } = require("pg");
 
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+  DB_PORT: process.env.DB_PORT,
+});
 
 module.exports = new Pool({
-  host: "localhost",
-  user: "<role_name>",
-  database: "top_users",
-  password: "<role_password>",
-  port: 5432
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
+
+
