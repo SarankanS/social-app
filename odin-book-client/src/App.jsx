67fa136f666b35d1cdcdc5a1a4d63fd5a356860d
Sarankan Srikaran <sarankansrikaran@gmail.com>
2026-01-27 14:28:05 -0500

    lift posts state and fetch posts on mount

--- odin-book-client/src/App.jsx
+++ odin-book-client/src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { CreatePost } from './components/CreatePost';
 import { Feed } from './components/Feed'
 
 export function App() {
+  const [posts, setPosts] = useState([]);
+    const fetchPosts = async () =>{  
+      try{
+          const res = await fetch('http://localhost:3000/api/posts');
+          if (!res.ok){
+              throw new Error("Failed to get Posts");
+          }
+          const data = await res.json();
+          console.log(data);
+          setPosts(data);
+  
+      }catch(err){
+          console.error("Failed to get Posts");
+      }
+    }
+    
+    useEffect(()=>{
+        fetchPosts();
+    }, []);
+
+  const addPost = (newPost) =>{
+    setPosts((prevPosts) => [newPost, ...prevPosts])
+  };
+  
 
 
   return (
     <>
       <h1>Hello</h1>
-      <CreatePost></CreatePost>
-      <Feed></Feed>
+      <CreatePost onPostCreated={addPost}> </CreatePost>
+      <Feed posts={posts}></Feed>
     </>
   );
 }
