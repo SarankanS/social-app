951d55d4cb339b00246034dc2b65a6b18de6e7be
Sarankan Srikaran <sarankansrikaran@gmail.com>
2026-01-27 14:29:14 -0500

    display updated posts from App

--- odin-book-client/src/components/Feed.jsx
+++ odin-book-client/src/components/Feed.jsx
@@ -1,29 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export function Feed(){
-
-    const [posts, setPosts] = useState([]);
-    
-    const fetchPosts = async () =>{
-        
-        try{
-            const res = await fetch('http://localhost:3000/api/posts');
-            if (!res.ok){
-                throw new Error("Failed to get Posts");
-            }
-            const data = await res.json();
-            console.log(data);
-            setPosts(data);
-    
-        }catch(err){
-            console.error("Failed to get Posts");
-        }
-    }
-
-    useEffect(()=>{
-        fetchPosts();
-    }, []);
-
+export function Feed({posts}){
 
     return (
       <div>
